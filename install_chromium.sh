12dca060bab446b46d5982f8dfe9aa9de91575d3
inprodi-web-studio <proyectos@inprodi.mx>
2024-08-07 18:59:13 -0600

    command

---
+++ install_chromium.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Actualiza la lista de paquetes
+apt-get update
+
+# Instala las dependencias necesarias para Chromium
+apt-get install -y \
+    gconf-service \
+    libasound2 \
+    libatk1.0-0 \
+    libcups2 \
+    libdbus-1-3 \
+    libfontconfig1 \
+    libgbm1 \
+    libgconf-2-4 \
+    libgtk-3-0 \
+    libnspr4 \
+    libnss3 \
+    libx11-xcb1 \
+    libxcomposite1 \
+    libxcursor1 \
+    libxdamage1 \
+    libxi6 \
+    libxrandr2 \
+    libxss1 \
+    libxtst6 \
+    ca-certificates \
+    fonts-liberation \
+    libappindicator1 \
+    libnss3 \
+    lsb-release \
+    xdg-utils \
+    wget
+
+# Instala Chromium
+apt-get install -y chromium-browser
