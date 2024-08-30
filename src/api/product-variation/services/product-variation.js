197c262d3633bbbbee01f08edcf56fbf6c7d4450
inprodi-web-studio <proyectos@inprodi.mx>
2024-08-30 17:21:39 -0600

    fix bug

--- src/api/product-variation/services/product-variation.js
+++ src/api/product-variation/services/product-variation.js
@@ -8,18 +8,6 @@ module.exports = createCoreService(PRODUCT_VARIATION, ({ strapi }) => ({
     async validateParallelData( data ) {
         let name = "";
 
-        for ( let i = 0; i < data.values.length; i++ ) {
-            const { id : valueId, name : valueName } = await findOneByUuid( data.values[i], ATTRIBUTE_VALUE );
-
-            data.values[i] = valueId;
-
-            if ( name ) {
-                name = `${name} / ${valueName}`;
-            } else {
-                name = valueName;
-            }
-        }
-
         await checkForDuplicates( PRODUCT_VARIATION, [
             {
                 sku : data.sku,
