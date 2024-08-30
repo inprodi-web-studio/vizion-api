7ed0bccd3d7c1ad98c960840db45e6e71ba7c6e8
inprodi-web-studio <proyectos@inprodi.mx>
2024-08-30 15:57:36 -0600

    db fix

--- config/database.js
+++ config/database.js
@@ -12,6 +12,7 @@ module.exports = ({ env }) => {
         database: env("DATABASE_NAME", "strapi"),
         user: env("DATABASE_USERNAME", "strapi"),
         password: env("DATABASE_PASSWORD", "strapi"),
+        connectTimeout: 60000,
         ssl: env.bool("DATABASE_SSL", false) && {
           key: env("DATABASE_SSL_KEY", undefined),
           cert: env("DATABASE_SSL_CERT", undefined),
