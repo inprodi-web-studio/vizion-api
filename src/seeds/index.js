93786f2abbab4cb29350a0abe889e6b3391f910c
inprodi-web-studio <proyectos@inprodi.mx>
2024-09-10 16:37:50 -0600

    rollback seeders

--- src/seeds/index.js
+++ src/seeds/index.js
@@ -1,7 +1,5 @@
 const {
     ROLE,
-    USER,
-    PRODUCT,
 } = require("../constants/models");
 
 const generateRoles = require("./roles");
@@ -9,52 +7,28 @@ const generateRoles = require("./roles");
 const generateSeeds = async (strapi) => {
     console.log("Seed data has started...");
 
-    // console.log("Deleting old data...");
+    console.log("Deleting old data...");
 
-    // const contentTypes = [
-    //     ROLE,
-    // ];
+    const contentTypes = [
+        ROLE,
+    ];
 
-    // for (const contentType of contentTypes) {
-    //     console.log(`Deleting ${contentType}...`);
+    for (const contentType of contentTypes) {
+        console.log(`Deleting ${contentType}...`);
     
-    //       await strapi.query(contentType).deleteMany({
-    //         where : {
-    //             id : {
-    //                 $not : null
-    //             },
-    //         },
-    //     });
-    // }
-
-    // console.log("Old data has been deleted!");
-
-    console.log("Generating new data...");
-
-    const companyProducts = await strapi.query( PRODUCT ).findMany({
-        where : {
-            company : {
-                uuid : "a8513a90-326f-4584-bc40-4ad4ba0d10e4",
-            },
-            unity : {
-                $null : true,
-            },
-        },
-    });
-
-    console.log( companyProducts );
-
-    for ( const product of companyProducts ) {
-        await strapi.entityService.update( PRODUCT, product.id, {
-            data : {
-                unity : 1,
+          await strapi.query(contentType).deleteMany({
+            where : {
+                id : {
+                    $not : null
+                },
             },
         });
     }
 
-    console.log("FINISHED!");
+    console.log("Old data has been deleted!");
 
-    // await generateRoles(strapi);
+    console.log("Generating new data...");
+    await generateRoles(strapi);
 }
 
 module.exports = generateSeeds;
