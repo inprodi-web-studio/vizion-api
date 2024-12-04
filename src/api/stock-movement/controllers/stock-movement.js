a89922f724a4b985638fd1aee436ffe5c1b128d1
Inprodi Web Studio <proyectos@inprodi.mx>
2024-12-04 14:13:44 -0600

    stock movements route

--- src/api/stock-movement/controllers/stock-movement.js
+++ src/api/stock-movement/controllers/stock-movement.js
@@ -1,11 +1,75 @@
-const { STOCK_MOVEMENT, STOCK } = require("../../../constants/models");
+const { STOCK_MOVEMENT, STOCK, STOCK_LOCATION } = require("../../../constants/models");
+const findMany = require("../../../helpers/findMany");
+const findOneByUuid = require("../../../helpers/findOneByUuid");
 const { validateCreateAdjustment, validateCreateReubication } = require("../content-types/stock-movement/stock-movement.validation");
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
+const movementsField = {
+    fields : ["uuid", "quantity", "type", "comments", "packageQuantity", "positionPartition", "createdAt"],
+    populate : {
+        product : {
+            fields : ["uuid", "name", "sku", "type"],
+        },
+        motive : {
+            fields : ["uuid", "title"],
+        },
+        badge : {
+            fields : ["uuid", "name", "expirationDate"],
+        },
+        variation : {
+            fields : ["uuid", "name", "sku"],
+            populate : {
+                values : {
+                    fields : ["uuid", "name"],
+                    populate : {
+                        attribute : {
+                            fields : ["uuid", "name"],
+                        },
+                    },
+                }
+            },
+        },
+        unity : {
+            fields : ["uuid", "name", "abbreviation"],
+        },
+        package : {
+            fields : ["uuid", "conversionRate", "realConversion"],
+            populate : {
+                unity : {
+                    fields : ["uuid", "name", "abbreviation"],
+                }
+            },
+        },
+        position : {
+            fields : ["uuid", "xPosition", "yPosition", "rotation", "partitions"],
+            populate : {
+                shelf : {
+                    fields : ["uuid", "name", "xPositions", "yPositions"],
+                },
+            }
+        },
+    },
+};
+
 module.exports = createCoreController(STOCK_MOVEMENT, ({ strapi }) => ({
     async find(ctx) {
+        const { locationUuid } = ctx.params;
 
+        const location = await findOneByUuid( locationUuid, STOCK_LOCATION );
+
+        const filters = {
+            $search : [
+                "product.name",
+                "product.sku",
+                "variation.sku",
+            ],
+            location : location.id,
+        };
+
+        const movements = await findMany( STOCK_MOVEMENT, movementsField, filters );
+
+        return movements;
     },
 
     async createAdjustment(ctx) {
--- src/api/stock-movement/routes/01-stock-movement.routes.js
+++ src/api/stock-movement/routes/01-stock-movement.routes.js
@@ -1,6 +1,14 @@
 module.exports = {
     routes: [
       {
+        method: "GET",
+        path: "/warehouses/:uuid/stock-locations/:locationUuid/stock-movements",
+        handler: "stock-movement.find",
+        config: {
+          policies: ["global::userContext"],
+        },
+      },
+      {
         method: "POST",
         path: "/stock-movements/adjustment",
         handler: "stock-movement.createAdjustment",
