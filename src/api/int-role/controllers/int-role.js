fbc4245c95e57373e3bf41debe87c716ef7d8410
Inprodi Web Studio <proyectos@inprodi.mx>
2025-01-09 18:27:07 -0600

    fix

--- src/api/int-role/controllers/int-role.js
+++ src/api/int-role/controllers/int-role.js
@@ -4,6 +4,32 @@ const { NotFoundError } = require('../../../helpers/errors');
 const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController( INT_ROLE, ({ strapi }) => ({
+    async getAppPermissions(ctx) {
+        console.log("app permissions");
+        const { app } = ctx.params;
+        const user = ctx.state.user;
+
+        const { intRole } = user;
+
+        if ( !intRole ) {
+            throw new NotFoundError("Role not found", {
+                key : "intRole.notFound",
+                path : ctx.request.path,
+            });
+        }
+
+        const permissions = intRole.permissions[app];
+
+        if ( !permissions ) {
+            throw new NotFoundError("Invalid route provided", {
+                key : "intRole.invalidRoute",
+                path : ctx.request.path,
+            });
+        }
+
+        return permissions;
+    },
+
     async getModulePermissions(ctx) {
         const { app, module } = ctx.params;
         const user = ctx.state.user;
@@ -18,33 +44,6 @@ module.exports = createCoreController( INT_ROLE, ({ strapi }) => ({
         }
 
         const permissions = intRole.permissions[app]?.[module];
-
-        if ( !permissions ) {
-            throw new NotFoundError("Invalid route provided", {
-                key : "intRole.invalidRoute",
-                path : ctx.request.path,
-            });
-        }
-
-        return permissions;
-    },
-
-    async getAppPermissions(ctx) {
-        const { app } = ctx.params;
-        const user = ctx.state.user;
-
-        const { intRole } = user;
-
-        if ( !intRole ) {
-            throw new NotFoundError("Role not found", {
-                key : "intRole.notFound",
-                path : ctx.request.path,
-            });
-        }
-
-        console.log(intRole.permissions);
-
-        const permissions = intRole.permissions[app];
 
         if ( !permissions ) {
             throw new NotFoundError("Invalid route provided", {
--- src/api/int-role/routes/01-int-role.routes.js
+++ src/api/int-role/routes/01-int-role.routes.js
@@ -2,16 +2,16 @@ module.exports = {
     routes : [
       {
         method  : "GET",
-        path    : "/int-roles/:app",
-        handler : "int-role.getAppPermissions",
+        path    : "/int-roles/:app/:module",
+        handler : "int-role.getModulePermissions",
         config : {
           policies : ["global::userContext"],
         },
       },
       {
         method  : "GET",
-        path    : "/int-roles/:app/:module",
-        handler : "int-role.getModulePermissions",
+        path    : "/int-roles/:app",
+        handler : "int-role.getAppPermissions",
         config : {
           policies : ["global::userContext"],
         },
