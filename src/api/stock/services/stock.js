ac7ebcd3a85e997fc0d44cd80180697f8051a5df
Inprodi Web Studio <proyectos@inprodi.mx>
2026-02-12 11:18:27 -0600

    variations stock fix

--- src/api/stock/services/stock.js
+++ src/api/stock/services/stock.js
@@ -1,55 +1,55 @@
-const { STOCK } = require('../../../constants/models');
+const { STOCK } = require("../../../constants/models");
 
-const { createCoreService } = require('@strapi/strapi').factories;
+const { createCoreService } = require("@strapi/strapi").factories;
 
 module.exports = createCoreService(STOCK, ({ strapi }) => ({
-    async formatStockData(data) {
-
+  async formatStockData(data) {
     let parsedData = [];
 
     data.forEach((item) => {
-    const variations = item.stocks?.filter((stock) => stock.variation?.uuid);
+      const variations = item.stocks?.filter((stock) => stock.variation?.uuid);
 
-    if (variations?.length > 0) {
+      if (variations?.length > 0) {
         parsedData.push({
-        hasVariation: true,
-        ...item,
+          hasVariation: true,
+          ...item,
         });
 
         let variationsMap = {};
 
         variations.forEach((variationStock) => {
-        const variationUuid = variationStock.variation.uuid;
+          const variationUuid = variationStock.variation.uuid;
 
-        if (!variationsMap[variationUuid]) {
+          if (!variationsMap[variationUuid]) {
             variationsMap[variationUuid] = {
-            product: item.product,
-            variation: {
+              product: item.product,
+              variation: {
                 uuid: variationUuid,
-                sku : variationStock.variation.sku,
-                values : variationStock.variation.values,
+                sku: variationStock.variation.sku,
+                values: variationStock.variation.values,
                 stocks: [],
-            },
+              },
             };
-        }
+          }
 
-        variationsMap[variationUuid].variation.stocks.push({
+          variationsMap[variationUuid].variation.stocks.push({
             badge: variationStock.badge,
             unity: variationStock.unity,
             package: variationStock.package,
             quantity: variationStock.quantity,
             packageQuantity: variationStock.packageQuantity,
-        });
+            reservations: variationStock.reservations ?? [],
+          });
         });
 
         Object.values(variationsMap).forEach((variationItem) => {
-            parsedData.push(variationItem);
+          parsedData.push(variationItem);
         });
-    } else {
+      } else {
         parsedData.push(item);
-    }
+      }
     });
 
-return parsedData;
-    },
+    return parsedData;
+  },
 }));
